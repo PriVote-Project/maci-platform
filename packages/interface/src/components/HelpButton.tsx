8521678c2e9a3adfa02ae0593f660114b88c0de6
lordforever <shashanktrivedi1917@gmail.com>
2025-10-07 22:32:25 +0530

    feat: gg24 deployment, add Arbitrum Sepolia deployment and update UI for dark mode and round states

--- packages/interface/src/components/HelpButton.tsx
+++ packages/interface/src/components/HelpButton.tsx
@@ -1,6 +1,7 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import Image from "next/image";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 import React from "react";
 import { CiCircleQuestion } from "react-icons/ci";
 
@@ -11,38 +12,42 @@ function closeDropdownMenu() {
   document.dispatchEvent(new KeyboardEvent("keydown", { key: "Escape" }));
 }
 
-export const HelpButton = (): JSX.Element => (
-  <DropdownMenu.Root>
-    <DropdownMenu.Trigger asChild>
-      <IconButton className="w-[100px] px-0  text-gray-600" icon={CiCircleQuestion} variant="ghost">
-        Help
-      </IconButton>
-    </DropdownMenu.Trigger>
+export const HelpButton = (): JSX.Element => {
+  const { theme } = useTheme();
 
-    <DropdownMenu.Portal>
-      <DropdownMenu.Content className="DropdownMenuContent" sideOffset={25}>
-        <DropdownMenu.Item className="DropdownMenuItem">
-          <Link className="w-full underline" href="/#FAQ" onClick={closeDropdownMenu}>
-            FAQ
-          </Link>
-        </DropdownMenu.Item>
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <IconButton className="w-[100px] px-0  text-gray-600" icon={CiCircleQuestion} variant="ghost">
+          Help
+        </IconButton>
+      </DropdownMenu.Trigger>
 
-        <DropdownMenu.Item className="DropdownMenuItem">
-          <Link className="w-full underline" href="/#Glossary" onClick={closeDropdownMenu}>
-            Glossary
-          </Link>
-        </DropdownMenu.Item>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content className={`DropdownMenuContent ${theme === "dark" ? "dark" : ""}`} sideOffset={25}>
+          <DropdownMenu.Item className="DropdownMenuItem">
+            <Link className="w-full underline" href="/#FAQ" onClick={closeDropdownMenu}>
+              FAQ
+            </Link>
+          </DropdownMenu.Item>
 
-        <DropdownMenu.Item className="DropdownMenuItem">
-          <Link className="w-full underline" href={feedbackUrl} rel="noreferrer" target="_blank">
-            Share your feedback
-          </Link>
+          <DropdownMenu.Item className="DropdownMenuItem">
+            <Link className="w-full underline" href="/#Glossary" onClick={closeDropdownMenu}>
+              Glossary
+            </Link>
+          </DropdownMenu.Item>
 
-          <div className="RightSlot">
-            <Image alt="arrow-go-to" className="dark:invert" height="15" src="/arrow-go-to.svg" width="18" />
-          </div>
-        </DropdownMenu.Item>
-      </DropdownMenu.Content>
-    </DropdownMenu.Portal>
-  </DropdownMenu.Root>
-);
+          <DropdownMenu.Item className="DropdownMenuItem">
+            <Link className="w-full underline" href={feedbackUrl} rel="noreferrer" target="_blank">
+              Share your feedback
+            </Link>
+
+            <div className="RightSlot">
+              <Image alt="arrow-go-to" className="dark:invert" height="15" src="/arrow-go-to.svg" width="18" />
+            </div>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
+};
